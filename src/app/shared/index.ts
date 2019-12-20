2a4363a5586560ac58c3a954196eb7dfdfe24c3b
Sathiyendren Rajendran <SaRajendran@manh.com>
2019-12-20 09:37:58 +0530

    Auto-generated commit

--- src/app/shared/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './util/request-util';
-export * from './util/constant-util';
-export * from './util/component-util';
-export * from './util/service-util';
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://knowmypeople.com/api',
-  configUrl: 'https://knowmypeople.com/content/KMPConfig.json'
-};
