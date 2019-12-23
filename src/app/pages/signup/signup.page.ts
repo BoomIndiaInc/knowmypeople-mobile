c736421b464ab4873c503dea4f7b0849629d48cc
Sathiyendren Rajendran <SaRajendran@manh.com>
2019-12-23 18:59:24 +0530

    Signup loading added

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -31,23 +31,28 @@ export class SignupPage implements OnInit {
     public formBuilder: FormBuilder,
     public componentUtil: ComponentUtil
   ) {
-    this.signupForm = formBuilder.group({
-      "login": ['', Validators.required],
-      "email": ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
-      "password": ['', Validators.compose([Validators.minLength(4), Validators.required])],
-      "confirm-password": ['', Validators.compose([Validators.minLength(4), Validators.required])],
-      "langKey": ['en', Validators.required]
-    }, {
-      validator: MustMatch('password', 'confirm-password')
-  });
+    this.signupForm = formBuilder.group(
+      {
+        login: ['', Validators.required],
+        email: ['', Validators.compose([Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')])],
+        password: ['', Validators.compose([Validators.minLength(4), Validators.required])],
+        'confirm-password': ['', Validators.compose([Validators.minLength(4), Validators.required])],
+        langKey: ['en', Validators.required]
+      },
+      {
+        validator: MustMatch('password', 'confirm-password')
+      }
+    );
 
-    this.translateService.get(['SIGNUP_ERROR', 'SIGNUP_SUCCESS', 'EXISTING_USER_ERROR', 'INVALID_PASSWORD_ERROR', 'NETWORK_ERROR']).subscribe(values => {
-      this.signupErrorString = values.SIGNUP_ERROR;
-      this.signupSuccessString = values.SIGNUP_SUCCESS;
-      this.existingUserError = values.EXISTING_USER_ERROR;
-      this.invalidPasswordError = values.INVALID_PASSWORD_ERROR;
-      this.networkError = values.NETWORK_ERROR;
-    });
+    this.translateService
+      .get(['SIGNUP_ERROR', 'SIGNUP_SUCCESS', 'EXISTING_USER_ERROR', 'INVALID_PASSWORD_ERROR', 'NETWORK_ERROR'])
+      .subscribe(values => {
+        this.signupErrorString = values.SIGNUP_ERROR;
+        this.signupSuccessString = values.SIGNUP_SUCCESS;
+        this.existingUserError = values.EXISTING_USER_ERROR;
+        this.invalidPasswordError = values.INVALID_PASSWORD_ERROR;
+        this.networkError = values.NETWORK_ERROR;
+      });
   }
 
   ngOnInit() {}
@@ -57,36 +62,38 @@ export class SignupPage implements OnInit {
   }
 
   doSignup() {
-    // set login to same as email
-    const signupFormValue: any = this.signupForm.value;
-    delete signupFormValue['confirm-password'];
-    // Attempt to login in through our User service
-    this.userService.signup(signupFormValue).subscribe(
-      async () => {
-        this.componentUtil.showToast(this.signupSuccessString, { cssClass: 'toast-success'}, true);
-        this.signupForm.reset();
-      },
-      async response => {
-        // Unable to sign up
-        const error = JSON.parse(response.error);
-        let displayError = this.signupErrorString;
-        if (response.status === 400 && error.type.includes('already-used')) {
-          displayError = this.existingUserError;
-        } else if (
-          response.status === 400 &&
-          error.message === 'error.validation' &&
-          error.fieldErrors[0].field === 'password' &&
-          error.fieldErrors[0].message === 'Size'
-        ) {
-          displayError = this.invalidPasswordError;
-        } else if (
-          error.message === 'error.network'
-        ){
-          displayError = this.networkError;
+    this.componentUtil.showLoading(() => {
+      // set login to same as email
+      const signupFormValue: any = this.signupForm.value;
+      delete signupFormValue['confirm-password'];
+      // Attempt to login in through our User service
+      this.userService.signup(signupFormValue).subscribe(
+        async () => {
+          this.componentUtil.showToast(this.signupSuccessString, { cssClass: 'toast-success' }, true);
+          this.signupForm.reset();
+          this.componentUtil.hideLoading();
+        },
+        async response => {
+          // Unable to sign up
+          const error = JSON.parse(response.error);
+          let displayError = this.signupErrorString;
+          if (response.status === 400 && error.type.includes('already-used')) {
+            displayError = this.existingUserError;
+          } else if (
+            response.status === 400 &&
+            error.message === 'error.validation' &&
+            error.fieldErrors[0].field === 'password' &&
+            error.fieldErrors[0].message === 'Size'
+          ) {
+            displayError = this.invalidPasswordError;
+          } else if (error.message === 'error.network') {
+            displayError = this.networkError;
+          }
+          this.componentUtil.showToast(displayError, { cssClass: 'toast-fail' }, true);
+          this.componentUtil.hideLoading();
         }
-        this.componentUtil.showToast(displayError, { cssClass: 'toast-fail' }, true);
-      }
-    );
+      );
+    }, 'REGISTERING');
   }
 
   onOpenLogin() {}
